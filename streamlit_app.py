9141ff50372dfff06478e8e14822e1104a2fbb54
William Kohlbeck <102195085+W-Kohlbeck@users.noreply.github.com>
2023-07-11 12:13:39 -0500

    Update streamlit_app.py
    changed fruit column to be the selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Adding a pick list so customers can pick the fruit to include in smoothie
-sl.multiselect("Pick some Fruits:", list(my_fruit_list.index))
+sl.multiselect("Pick some Fruits:", list(my_fruit_list.fruit))
 
 # Display table on page
 sl.dataframe(my_fruit_list)
